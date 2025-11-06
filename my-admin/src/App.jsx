fb214b8d486c1062e0cdc969153b2ddc48748634
jade-benting <27137120@sun.ac.za>
2025-11-06 11:11:35 +0000

    Setting up react-admin

---
+++ my-admin/src/App.jsx
@@ -0,0 +1,19 @@
+// in src/admin/index.tsx
+import { Admin, Resource, ListGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={ListGuesser} />
+    <Resource name="comments" list={ListGuesser} />
+  </Admin>
+);
+
+
+import MyAdmin from "./admin";
+
+const App = () => <MyAdmin />;
+
+export default App;
